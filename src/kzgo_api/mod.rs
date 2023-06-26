b3c76329fab5275f9f5810eacffe75a5909a5e4b
AlphaKeks <alphakeks@dawn.sh>
2023-06-26 18:29:05 +0200

    feat(kzgo-api): add a bunch of functions

--- src/kzgo_api/mod.rs
+++ src/kzgo_api/mod.rs
@@ -1,3 +1,11 @@
+use crate::{
+	error::{Error, Result},
+	http::get_json,
+	prelude,
+	types::SteamID,
+	utils::EmptyParams,
+};
+
 /// The base URL for all API requests.
 pub const BASE_URL: &str = "https://kzgo.eu/api";
 
@@ -19,4 +27,59 @@ pub use servers::Server;
 
 /// The `/wrs` routes
 pub mod world_records;
-pub use world_records::WorldRecord;
+pub use world_records::{get_wrs, leaderboards, WorldRecord};
+
+/// Get a player's completions for a given mode
+#[tracing::instrument(level = "INFO", skip(client), err(Debug))]
+pub async fn get_completions<M>(mode: M, client: &crate::Client) -> Result<Completions>
+where
+	M: Into<prelude::Mode> + std::fmt::Debug,
+{
+	get_json(&format!("{BASE_URL}/completions/{}", mode.into().api()), &EmptyParams, client).await
+}
+
+/// Fetches maps
+#[tracing::instrument(level = "INFO", skip(client), err(Debug))]
+pub async fn get_maps(client: &crate::Client) -> Result<Vec<Map>> {
+	let maps: Vec<_> = get_json(&format!("{BASE_URL}/maps"), &EmptyParams, client).await?;
+
+	if maps.is_empty() {
+		return Err(Error::EmptyResponse);
+	}
+
+	Ok(maps)
+}
+
+/// Fetches a single map
+#[tracing::instrument(level = "INFO", skip(client), err(Debug))]
+pub async fn get_map(map_name: &str, client: &crate::Client) -> Result<Vec<Map>> {
+	let maps: Vec<_> =
+		get_json(&format!("{BASE_URL}/maps/{map_name}"), &EmptyParams, client).await?;
+
+	if maps.is_empty() {
+		return Err(Error::EmptyResponse);
+	}
+
+	Ok(maps)
+}
+
+/// Fetches information about a player
+#[tracing::instrument(level = "INFO", skip(client), err(Debug))]
+pub async fn get_steam_user<S>(steam_id: S, client: &crate::Client) -> Result<User>
+where
+	S: Into<SteamID> + std::fmt::Debug,
+{
+	get_json(&format!("{BASE_URL}/steam/{}", steam_id.into().as_id64()), &EmptyParams, client).await
+}
+
+/// Fetches all servers from the "servers" tab on kzgo.eu
+#[tracing::instrument(level = "INFO", skip(client), err(Debug))]
+pub async fn get_servers(client: &crate::Client) -> Result<Vec<Server>> {
+	let servers: Vec<_> = get_json(&format!("{BASE_URL}/servers"), &EmptyParams, client).await?;
+
+	if servers.is_empty() {
+		return Err(Error::EmptyResponse);
+	}
+
+	Ok(servers)
+}
